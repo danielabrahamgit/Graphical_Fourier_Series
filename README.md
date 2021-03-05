d34c46e185ea1f700b85cc60b0b298680c501f7b
Daniel Abraham <daniel_abraham1@berkeley.edu>
2021-03-05 00:34:47 -0800

    Made the code more user friendly, added picture in README

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# Graphical_Fourier_Series
-We will be developing a graphical tool for viewing the Fourier series of any real function.
+## Inspiration
+I will be developing a graphical tool for viewing the Fourier series of any real function, this is inspired by [this video](https://www.youtube.com/watch?v=ds0cmAV-Yek). I thought that the creator of this visualitzation technique has come across a very elegant way of displaying/demonstrating the fourier series. My attemp to recreate this is out of passion and desire to have more individuals be able to customize the code so that they can make their own demonstrations.  
+
+## Dependencies
+The entire thing is run in python, you will need the following libraries: 
+1. **numpy**:  
+    `pip install numpy`
+2. **scipy**:  
+    `pip install scipy`
+3. **matplotlib**:  
+    `pip install matplotlib`
+4. **arcade**:  
+    `pip install arcade`
+
+## Results
+Here is an example of a square wave signal being represented by ~ 20 fourier series coefficients:
+
+![alt text](square.png)
--- main.py
+++ main.py
@@ -13,7 +13,7 @@ TITLE  = "Fourier Series Demo"
 # Sampling frequency
 Fs = 1000
 # Number of fourier coefficients
-num_coef = 50
+num_coef = 20
 # Time interval
 t_start = -1
 t_end   = 1.0
@@ -24,7 +24,7 @@ w0 = 2 * np.pi / P
 # NUMBER of SAMPLES in thhe OUTPUT signal
 NSO = 300
 # Output signal buffer
-sig_pts = np.array([[-NSO, -NSO]] * NSO)
+sig_pts = np.array([[WIDTH, HEIGHT/2]] * NSO)
 
 # time array
 t = np.linspace(t_start, t_end, int(P * Fs))
@@ -45,31 +45,42 @@ def complx_to_pixel(z):
 # Used to move each draw forward by some amount of time
 time_index = 0
 
+# Length of output in time, you can fill these in
+t_output = np.linspace(-10, 10, int(P * Fs))
+
+# amount to increment time each frame call
+# NOTE: the output buffer will only update whenever
+#       the time_index is an integer
+inc_amount = 0.1
+
 # Drawing function, executed on each frame
 def on_draw(delta_time):
-    global i, sig_pts
+    global time_index, sig_pts
     arcade.start_render()
 
     # Draw all the complex vectors
     z_prev = -1.2 + 0j
     z_curr = 0 + 0j
     for k in range(len(c)):
-        z_curr = np.exp(1j * w0 * k * t[i]) * c[k] * -1j + z_prev
+        z_curr = np.exp(1j * w0 * k * t_output[int(time_index)]) * c[k] * -1j + z_prev
         x0, y0 = complx_to_pixel(z_prev)
         x1, y1 = complx_to_pixel(z_curr)
         arcade.draw_line(x0, y0, x1, y1, arcade.color.WHITE, 1)
         arcade.draw_circle_outline(x0, y0, np.sqrt((x1 - x0) ** 2 + (y1 - y0)**2), arcade.color.ENGLISH_RED, 0.4)
         z_prev = z_curr
     
-    # Last iteration ---> we have the tip stored in z-curr
+    # Last iteration ---> we have the tip stored in z-curr.
+    # Use this fact to draw out the signal by appending the tip
+    if (abs(time_index - int(time_index)) < inc_amount):
+        sig_pts += [1, 0]
+        sig_pts = np.vstack((np.array([[WIDTH - NSO, y1]]), sig_pts[:-1,:]))
     arcade.draw_line(x1, y1, WIDTH - NSO, y1, arcade.color.WHITE, 1)
-    sig_pts += [1, 0]
-    sig_pts = np.vstack((np.array([[WIDTH - NSO, y1]]), sig_pts[:-1,:]))
-    arcade.draw_points(sig_pts, arcade.color.WHITE, 1)
+    arcade.draw_line_strip(sig_pts, arcade.color.WHITE, 1)
+
     # increment the time
-    i += 1
-    if i >= len(t):
-        i = 0
+    time_index += inc_amount
+    if time_index >= len(t_output):
+        time_index = 0
 
 # Setup rendering/drawing
 arcade.open_window(WIDTH, HEIGHT, TITLE)
